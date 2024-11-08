85eee43b6e427fe391b53d5dd1e99d386e5337cd
Liconmwangi <lincolnmwangi56@gmail.com>
2024-11-08 20:50:26 +0300

    Create CAT2_Q2.c.cpp

---
+++ CAT2_Q2.c.cpp
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include<conio.h>
+int main() 
+{
+
+int i,j;	
+int scores[2][2]={{40,92},{84,72}};
+for(i=0;i<=2;i++) 
+
+	for(j=0;j<=2;j++)
+
+
+{
+	printf("%d",scores[i][j]);
+	{
+		printf("\n");	
+	}
+}
+getch();
+}
